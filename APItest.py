1caf17578046416b3af8e5e1634b4ba703d8e5c8
Sachit Jain <jsachit@gmail.com>
2018-07-31 18:57:18 -0400

    s

--- APItest.py
+++ APItest.py
@@ -3,4 +3,4 @@ url = ('https://newsapi.org/v2/top-headlines?'
        'country=us&'
        'apiKey=94074cd9596f4e2a812c7e6eb286ac7f')
 response = requests.get(url)
-print response.json()
+response.json()
