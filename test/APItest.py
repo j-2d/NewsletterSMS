be9c3d16b23632e1e1881d3852c24c86e374a56e
j-2d <j.2sdale@gmail.com>
2018-08-07 17:51:56 -0400

    removed test folder and files

--- test/APItest.py
+++
@@ -1,6 +0,0 @@
-import requests
-url = ('https://newsapi.org/v2/top-headlines?'
-       'country=us&'
-       'apiKey=94074cd9596f4e2a812c7e6eb286ac7f')
-response = requests.get(url)
-response.json()
