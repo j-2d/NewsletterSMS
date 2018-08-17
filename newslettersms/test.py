f08c6811cbc0530098d7305bb4a8dc5b9e069c31
jsachit <jsachit@gmail.com>
2018-08-17 17:58:40 -0400

    F sobe pt. 2 (no API key)

--- newslettersms/test.py
+++ newslettersms/test.py
@@ -12,9 +12,8 @@ class Testmain(unittest.TestCase):
     def test_news_api(self):
         vals, vals_api = newslettersms.def_vars('../.conf', '../.env')
 
-        arraye = ['94074cd9596f4e2a812c7e6eb286ac7f', 1, 5]
-
-        self.assertEqual(vals_api['news'], arraye)
+        self.assertEqual(vals_api['news'][1], 1)
+        self.assertEqual(vals_api['news'][2], 5)
 
 if __name__ == '__main__':
     unittest.main()
